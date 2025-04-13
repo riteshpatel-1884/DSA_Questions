aa925dcf382aa9b8efd9731c11ec396875e13ea1
Ritesh Patel <129499361+riteshpatel-1884@users.noreply.github.com>
2025-04-13 12:08:07 +0530

    Update power.java

--- Recursion/power.java
+++ Recursion/power.java
@@ -9,8 +9,8 @@ class Solution {
         return true; //If found, it will return true;
 
         if(Math.pow(4, pow)>n)
-        return false;  // if power > number. Ex: n = 123. First 4,0 will be calculated then 4,1 then 4,2
-                          then 4,3 = 64 till there power is not found but when 4,4 is calcualted then it is = 256 > 123 therefore it will return false.
+        return false;  // if power > number. Ex: n = 123. First 4,0 will be calculated then 4,1 then 4,2 then 4,3 = 64 till there power is not found 
+        //but when 4,4 is calcualted then it is = 256 > 123 therefore it will return false.
 
         return power(n,pow+1); 
     }
