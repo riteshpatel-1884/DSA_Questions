b9a596fa97ac190ae26cf6d20ca29d1c52422b37
Ritesh Patel <129499361+riteshpatel-1884@users.noreply.github.com>
2025-05-28 22:37:48 +0530

    Update 7. Find the node where the cycle begins.java

--- Linked List/7. Find the node where the cycle begins.java
+++ Linked List/7. Find the node where the cycle begins.java
@@ -1,1 +1,29 @@
 
+
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode temp = head;
+        ListNode slow = head;
+        ListNode fast = head;
+
+        if (head == null || head.next == null)
+            return null;
+
+        while (fast != null) {
+            if (slow.next == null)
+                return null;
+            slow = slow.next;
+            if (fast.next == null)
+                return null;
+            fast = fast.next.next;
+            if (slow == fast) {
+                while (slow != temp) {
+                    temp = temp.next;
+                    slow = slow.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
+}
