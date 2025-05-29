eae6f86b63899995d8a28607e1ddf8981ba97716
Ritesh Patel <129499361+riteshpatel-1884@users.noreply.github.com>
2025-05-29 22:07:55 +0530

    Update 8. Merge two sorted LL.java

--- Linked List/8. Merge two sorted LL.java
+++ Linked List/8. Merge two sorted LL.java
@@ -1,33 +1,29 @@
-//Using Extra Space
+
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         ListNode temp1 = list1;
         ListNode temp2 = list2;
-        ListNode head = new ListNode(100);
-        ListNode temp = head;
+        ListNode temp = new ListNode(100);
+        ListNode ans = temp;
 
-        while(temp1 != null && temp2 != null) {
-            if(temp1.val < temp2.val) {
-                ListNode a = new ListNode(temp1.val);
-                temp.next = a;
-                temp = a;
+        while(temp1!=null && temp2!=null){
+            if(temp1.val<temp2.val){
+                ans.next = temp1;
                 temp1 = temp1.next;
-            } else {
-                ListNode a = new ListNode(temp2.val);
-                temp.next = a;
-                temp = a;
+                ans = ans.next;
+            }
+            else{
+                ans.next = temp2;
                 temp2 = temp2.next;
+                ans = ans.next;
             }
         }
-if (temp1 == null) {
-    temp.next = temp2;
-} else {
-    temp.next = temp1;
-}
-return head.next;
+        if(temp1==null){
+             ans.next = temp2;
+        }
+        if(temp2==null){
+                ans.next = temp1;
+        }
+        return temp.next;
     }
 }
-
-
-
-// Without using extra space
